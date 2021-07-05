c9f9105e68f329b4c734f09bbdb2b4a4f2029844
martinnolan <martinnolan73517@gmail.com>
2021-07-05 20:26:58 +0100

    1st commitment

---
+++ springboot-in-10-steps2/src/main/java/com/in28minutes/springboot/basics/springbootin10steps2/SpringbootIn10Steps2Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.basics.springbootin10steps2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIn10Steps2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIn10Steps2Application.class, args);
+	}
+
+}
---
+++ springboot-in-10-steps2/src/test/java/com/in28minutes/springboot/basics/springbootin10steps2/SpringbootIn10Steps2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.basics.springbootin10steps2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIn10Steps2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
